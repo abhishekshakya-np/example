699d97bd0774fa607cea22b090005e8de1148645
Abhishek shakya <abishek12344321@gmail.com>
2024-06-02 16:02:24 +0545

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Page</title>
+</head>
+<body>
+<nav>
+        <a href="/" class="home about">Home</a>
+        <a href="/about" class="home about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Hello from about page.
+    </h1>
+</body>
+</html>
