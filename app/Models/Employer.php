bbbc4c669df61481e9b600c106c150a73e7667b0
Abhishek Shakya <abhishek.shakya@ditagtech.com.au>
2025-09-08 00:38:37 +0545

    Add Employer model
    Create the Employer model with a relationship to jobs, allowing for the retrieval of job listings associated with each employer.

---
+++ app/Models/Employer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Employer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Get the jobs for the employer.
+     */
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
+}
